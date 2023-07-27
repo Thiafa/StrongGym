22738abd34c5a0fb5d67e724b561f4bb1188d300
Thiago <gerencia.desenvolvimento@unimontes.br>
2023-07-27 17:59:59 -0300

    Fix: envio de email utilizando api

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click',(e)=>{
+  e.preventDefault()
+  const nome = document.getElementById('nome').value
+  const email = document.getElementById('email').value
+  const telefone = document.getElementById('telefone').value
+  const mensagem = document.getElementById('mensagem').value
+  data = {
+    nome,
+    email,
+    telefone,
+    mensagem
+  }
+  enviarDados(data)
+})
+const enviarDados = async () =>{
+  try {
+    const dados = await axios.post('http://127.0.0.1:5000/send-email', data)
+  console.log(dados)
+    } catch (error) {
+    console.error('Erro',error);
+}
+}
+
